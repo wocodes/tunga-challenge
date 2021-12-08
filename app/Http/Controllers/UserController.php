149ab7a162c5b1ced24dd504859e201539f83589
william <william.odiomonafe@gmail.com>
2021-12-08 01:58:22 +0100

    - Created UserRepository class to contain main import operation and moved logic from UserController class to UserRepository.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,26 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Repository\UserRepository;
 use App\Jobs\ProcessImportJob;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use JsonMachine\JsonDecoder\ExtJsonDecoder;
+use JsonMachine\JsonDecoder\PassThruDecoder;
+use JsonMachine\JsonMachine;
 
 class UserController extends Controller
 {
+    public UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
 
     public function importData(Request $request)
     {
         // validate file
-//        $request->validate([
-//           'file' => 'file|mimes:json',
-//        ]);
+        $request->validate([
+           'file' => 'file|mimes:json,text',
+        ]);
 
-        // cache data
-        $data = json_decode($request->file->getContent(), true);
+        $this->userRepository->processImportRequest($request);
 
-        $chunkedData = array_chunk($data, 1000);
-
-        foreach ($chunkedData as $datum) {
-            ProcessImportJob::dispatch($datum);
-        }
     }
 }
